daffffbae5435346c616d144f57a38723ea0c07a
Alexander Whillas <whillas@gmail.com>
2025-06-13 06:31:17 +1200

    add humanize and remove some weird spinner stuff

--- pigeon/settings.py
+++ pigeon/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'django_bootstrap5',
     'django_htmx',
+    'django.contrib.humanize',
     
     # Local apps
     'stool',
